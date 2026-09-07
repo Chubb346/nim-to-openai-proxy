ac3626aa283b6e1232253844ff75d90a4170672a
Chubb346 <chubbtunes346@gmail.com>
2026-09-07 16:50:06 -0300

    Change ENABLE_THINKING_MODE to false by default

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3000;
 const NIM_API_BASE = process.env.NIM_API_BASE || 'https://integrate.api.nvidia.com/v1';
 const NIM_API_KEY = process.env.NIM_API_KEY;
 const CLIENT_AUTH_KEY = process.env.CLIENT_AUTH_KEY;
-const ENABLE_THINKING_MODE = process.env.ENABLE_THINKING_MODE === 'true';
+const ENABLE_THINKING_MODE = process.env.ENABLE_THINKING_MODE === 'false';
 const SKIP_VALIDATION = process.env.SKIP_VALIDATION === 'true';
 const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
 
